63d39ba62cef58e92d488d5bca3b6cf9f71af6ca
reihan35 <reihaneh77@yahoo.com>
2018-10-24 12:48:38 +0200

    tme5

--- tme1.py
+++ tme1.py
@@ -17,7 +17,7 @@ def sac_a_dos(R, T_max):
     return U, P, b
 
 def algo_glout(sac):
-    print("instance de sac : " , sac)
+    #print("instance de sac : " , sac)
     l2 = []
     psac = 0
     sac_fin = []
@@ -40,9 +40,52 @@ def algo_glout(sac):
         ind = elem[1]
         if psac + P[ind] < b:
            psac = psac + P[ind]
-           sac_fin.append(ind)
+           sac_fin.append((ind,1))
            
-    return sac_fin
+    u_sac_fin = 0
+    
+    for (i_obj, r_obj) in sac_fin:
+        u_sac_fin += U[i_obj] * r_obj
+        
+    return u_sac_fin
+
+def algo_glout_frac(sac):
+    #print("instance de sac : " , sac)
+    l2 = []
+    psac = 0
+    sac_fin = []
+    
+    T_max = len(sac[0])
+    U = sac[0]
+    P = sac[1]
+    b = sac[2]
+    
+    for i in range(T_max):
+        l2.append((U[i]/P[i],i)) 
+     
+
+
+    l2=sorted(l2, reverse=True)
+
+    #l2.sort(key=lambda x: x[0], reverse=True)
+
+    for elem in l2 :
+        ind = elem[1]
+        if psac + P[ind] <= b:
+           psac = psac + P[ind]
+           sac_fin.append((ind,1))
+        else:
+           r = ((b - psac) * 1.0) / P[ind]
+           psac = psac + P[ind] * r
+           sac_fin.append((ind, r))
+        
+    u_sac_fin = 0
+    
+    for (i_obj, r_obj) in sac_fin:
+        u_sac_fin += U[i_obj] * r_obj
+        
+    return u_sac_fin
+
 
 def b_and_b_1(sac):
     U = np.array(sac[0])
@@ -51,20 +94,37 @@ def b_and_b_1(sac):
     n = len(U)
     
     pile = []
-    
-    #current_solution = []
-    #current_p = []
-    #pile.append(current_solution)
-    
     binary_list = np.zeros(n)
     
     solutions = []
     pile = [(binary_list, 0)]
+    print(sac)
+    
+    best_s= []
+    best_v = 0 
+    b_sup = algo_glout_frac(sac)
+    b_inf = algo_glout(sac)
+    
+    print("b_sup, b_inf", b_sup, b_inf)
     
     while pile != []:
-      #  print(pile)
         current_blist, i = pile.pop()
-        #print(i)
+        
+        if i == 0:
+            ind = np.argwhere(current_blist == 1)
+            u_ind = U[ind]
+            p_ind = P[ind]
+            
+            marwan = np.argwhere(current_blist == 0)
+            b_sup = algo_glout_frac((U[marwan],P[marwan],b-p_ind.sum())) + u_ind.sum()
+            new_b_inf = algo_glout((U[marwan],P[marwan],b-p_ind.sum())) + u_ind.sum()
+            b_inf = min(new_b_inf, b_inf)
+            
+            print(b_inf, b_sup)
+            if b_sup < b_inf:
+                continue
+        
+        
         if i != n:
             new_blist = current_blist.copy()
             new_blist[i] = 1
@@ -72,43 +132,21 @@ def b_and_b_1(sac):
             pile.append((new_blist, i+1))
             pile.append((current_blist, i+1))
         else:
-             #print(pile)
-            solutions.append(current_blist)
+            s_u = np.array(U[np.argwhere(current_blist == 1.)]).sum()
+            s_p = np.array(P[np.argwhere(current_blist == 1.)]).sum()
             
-    best_u = 0
-    best_s = []
-
-
-    s_u = 0
-        smax = 0
-        pmax = 0
-        somme = 0
-        poids = 0
-    #print(solutions)
-    for s in solutions:
-        s = np.array(s)
-        #print(s, np.array(U[np.argwhere(s == 1.)]),np.array(P[np.where(s == 1.)]))
-        #s_u = np.array(U[np.argwhere(s == 1.)]).sum()
-        #s_p = np.array(P[np.argwhere(s == 1.)]).sum()
-        
-            for i in x :
-                somme = somme + U[i]
-                poids = poids + P[i]
-            if somme > smax:
-                smax = somme 
-        
-        print(s, s_u, s_p)
-        if s_p > b:
-            continue
-        else:
-            if best_u < s_u:
-                best_u = s_u
-                best_s = s
+            if s_p > b:
+                continue
+            if best_v < s_u:
+                best_s = current_blist
+                best_v = s_u
                 
-    return best_s
+            solutions.append(current_blist)
+          
+    return np.argwhere(best_s == 1)
             
-sac = sac_a_dos(10,2)
-print(sac)
-print(b_and_b_1(sac_a_dos(10,2)))    
-#print(sac_a_dos(10,2))
-#print(algo_glout(sac_a_dos(10, 5)))
+
+#sac = sac_a_dos(10,2)
+#print(sac)
+#print(algo_glout_frac(([4, 2], [7, 5], 6)))
+print(b_and_b_1(sac_a_dos(30,5)))    
