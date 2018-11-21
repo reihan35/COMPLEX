b8de0382a68f0b2dccc368d70875aef9eddf00c6
reihan35 <reihaneh77@yahoo.com>
2018-11-21 18:42:28 +0100

    bug fixed

--- projet.py
+++ projet.py
@@ -9,7 +9,9 @@ Created on Wed Nov 21 11:03:16 2018
 import numpy as np
 import matplotlib.pyplot as plt
 import time
-
+import random
+from math import *
+'''
 def divis_pos(n):
     l=[]
     for x in range(1,n+1):
@@ -17,32 +19,9 @@ def divis_pos(n):
             l.append(x)
 
     return l
+'''
 
-def my_gcd(a,b):
-    la=divis_pos(a)
-    lb=divis_pos(b)
-
-    lc=[]
-    if a == 1:
-        return b
-    if b == 1:
-        return a
-    
-    for x in la:
-        for y in lb:
-            if x==y:
-                lc.append(x)
-
-    return max(lc)
-
-def algo_euclide(a,b) :
-    if (b==0) :
-        return(a)
-    else :
-        r=a%b
-        return algo_euclide(b,r)
-
-
+'''
 def table(n):
     l=[]
     for x in range(0,n):
@@ -63,37 +42,97 @@ def my_inverse(n,a):
     return -1
 
 
-"""t_m = 0
-start_time = time.time()
-t_m = t_m + (time.time() - start_time)"""
+
+def my_inverse(a, n):
+    t = 0
+    newt = 1
+    r = n
+    newr = a
+    
+    while newr != 0:
+        quotient = r / newr
+        (t, newt) = (newt, t - quotient * newt)
+        (r, newr) = (newr, r - quotient * newr)
+    print(r)
+        
+    if r > 1:
+        return -1
+    if t < 0:
+        t = t + n
+    return t
+
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = egcd(b % a, a)
+        return (g, x - (b // a) * y, y)
+'''
+def my_inverse(a, m):
+    g, x, y = egcd(a, m)
+    if g != 1:
+        return -1
+    else:
+        return x % m
+
+def my_gcd(a,b) :
+    if (b==0) :
+        return(a)
+    else :
+        r=a%b
+        return algo_euclide(b,r)
+"""
+def l(n):
+    if log(n)/log(2) - int(log(n)/log(2)) == 0 : 
+        return [log(n)/log(2)]
+    else: 
+        return l(n-(2**int(log(n)/log(2)))).append(log(n)/log(2))'''
+
+print(l(25))  """
+     
+T_pgcd = []
+T_inverse = []
+
+i = 10
+x = []
+
+while i < 100000000000000000000000000000000000000000000000000000:
+#for b in [10**5, 10**10, 10**15, 10**20, 10**25]:
+    x.append(i)
+    a = random.randint(0,i)
+    b = random.randint(0,i)
+    start_time = time.time()
+    my_gcd(a,b)
+    T_pgcd.append(time.time() - start_time)
+    i += 1000000000000000000000000000000000000000000000000000
+
+plt.plot(x, T_pgcd)
+plt.show()
+
+
 
 T_pgcd = []
 T_inverse = []
-a = 5
-# b = 
 
-"""for n in range(10, 10000, 10):
-    start_time = time.time()
-    my_inverse(n,a)
-    T_inverse.append(time.time() - start_time)
-    
-plt.plot(np.arange(10,10000, 10), T_inverse)
-plt.show()
-"""
-print(algo_euclide(10000,86))
+i = 10
+x = []
 
-a = 100000000000000000000000000000000000000000000000000000
-print(a)
-start = 1000000000000000
-for b in range(start,a,100000000000000000000000000000):
+while i < 100000000000000000000000000000000000000000000000000000:
 #for b in [10**5, 10**10, 10**15, 10**20, 10**25]:
+    x.append(i)
+    a = random.randint(0,i)
+    b = random.randint(0,i)
     start_time = time.time()
-    algo_euclide(a,b)
+    my_inverse(a,b)
     T_pgcd.append(time.time() - start_time)
-    
-x = np.arange(start, a, 100000000000000000000000000000)
+    i += 1000000000000000000000000000000000000000000000000000
+
 plt.plot(x, T_pgcd)
 plt.show()
+
+
+
+
 #k = np.argmax(T_pgcd)
 #print(x[k], T_pgcd[k])
 
